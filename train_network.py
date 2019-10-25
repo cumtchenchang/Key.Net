ee380889f7f003519b1ee24b943b325c5a2acd65
chenchang(陈常) <1253419722@qq.com>
2019-10-25 21:31:38 +0800

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -327,8 +327,12 @@ def train_keynet_architecture():
                     tf.train.latest_checkpoint(checkpoint_file_path), variables_to_restore)
 
         # GPU Usage
-        config = tf.ConfigProto()
-        config.gpu_options.per_process_gpu_memory_fraction = args.gpu_memory_fraction
+        # config = tf.ConfigProto()
+        # config.gpu_options.per_process_gpu_memory_fraction = args.gpu_memory_fraction
+        config = tf.ConfigProto(allow_soft_placement=True)
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+        config.gpu_options.allow_growth = True
+
 
         with tf.Session(config=config) as sess:
 
